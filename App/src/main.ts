6b2b73d7de42ac2124eb29f0962fe2fe296b2b32
josuereydev <josuereydev@gmail.com>
2024-10-06 23:12:57 -0300

    commmite

--- App/src/main.ts
+++ App/src/main.ts
@@ -1,8 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module'; // Cam
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
-  const port = process.env.PORT || 3000; // Usa el puerto del entorno
-  await app.listen(port);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
